69670d799d7645670cc9a9a2f67c37c32a6d005d
L-A-Davis <lindsay.amanda.davis@gmail.com>
2018-01-12 17:22:01 -0500

    complete jQuery to javascript change

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+document.querySelector('#save_lincoln').addEventListener('click', saveLincoln2)
 
-function saveLincoln(interval) {
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
+
+function saveLincoln2() {
+  let interval = document.querySelector('#interval').value
   if(isNaN(interval)) {
     interval = 10000
+  } else {
+
+  let element = document.querySelector('#foreground')
+  element.style.opacity = 0;
+  element.style.transition = `opacity ${interval}ms`
+    }
   }
-  $('#foreground').fadeOut(interval);
-}
